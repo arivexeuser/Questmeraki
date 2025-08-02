012d15c22a4f3361071fa8e7ced0032f9938df90
Pinkypri <priya.agri1999@gmail.com>
2025-08-02 13:57:38 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://questmeraki.netlify.app'],
+  origin: ['http://localhost:5173','https://questmeraki.netlify.app','https://questmeraki.vercel.app','https://questmeraki.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
   credentials: true,
 }));
